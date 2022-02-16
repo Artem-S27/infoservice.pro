f7ac8a32da7a07cdddcf6296061c905036eb3ea0
Artem-S27 <54069536+Artem-S27@users.noreply.github.com>
2022-02-16 20:30:34 +0300

    description.php

---
+++ .description.php
@@ -0,0 +1,9 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$arComponentDescription = array(
+   "NAME" => "Новости по интересам",
+   "DESCRIPTION" => "Новости по интересам",
+   "PATH" => array(
+      "ID" => "Новости по интересам"
+   )
+);
+?>
